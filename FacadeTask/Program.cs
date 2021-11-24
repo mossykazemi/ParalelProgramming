7dd19a4056477d0ba33d7683f14685997b5736cb
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-11-24 23:02:21 +0330

    Facade Tasks used

---
+++ FacadeTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("*** Main Start ***");
+
+Run();
+
+Console.WriteLine("*** Main End ***");
+Console.ReadKey();
+
+static async void Run()
+{
+    var stopwatch = Stopwatch.StartNew();
+    var tcs = new TaskCompletionSource<bool>();
+    Console.WriteLine($"Start : {stopwatch.ElapsedMilliseconds} ms");
+
+    var fnf = Task.Delay(2000).ContinueWith(task => tcs.SetResult(true));
+    Console.WriteLine($"Waiting : {stopwatch.ElapsedMilliseconds} ms");
+
+    await tcs.Task;
+    Console.WriteLine($"Done in: {stopwatch.ElapsedMilliseconds} ms");
+    stopwatch.Stop();
+}
