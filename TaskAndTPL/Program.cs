e173572d63a4339deb5571d0b3a1d3e912d74f29
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-11-24 23:00:15 +0330

    Task and TPL

---
+++ TaskAndTPL/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("*** Main start ****");
+
+//Parallel.Invoke(() => Operation2(), () => Operation1());
+
+var task1 = Task.Run(() => Operation1());
+Console.WriteLine("this is execute first");
+task1.Wait();
+
+var task2 = Task.Run(() => Operation2());
+Console.WriteLine("this is execute second");
+task2.Wait();
+
+Console.WriteLine("*** Main end ****");
+Console.ReadKey();
+
+static void Operation1()
+{
+    Console.WriteLine("Operation 1 Executed");
+}
+
+static void Operation2()
+{
+    Console.WriteLine("Operation 2 Executed");
+}
