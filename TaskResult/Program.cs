d59281224f0456df48beed8e6f31101b080ecc06
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-11-26 21:17:08 +0330

    Task Result Used and tested

---
+++ TaskResult/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("*** Main Start ***");
+Run();
+Console.WriteLine("*** Main End ***");
+Console.ReadKey();
+
+static async void Run()
+{
+    var t1 = Task<string>.Run(() =>
+    {
+        Thread.Sleep(4000);
+        return "Task One Executed";
+    });
+
+    var taskResult = await t1;
+    Console.WriteLine(taskResult);
+}
