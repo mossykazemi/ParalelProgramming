50ce850c8724de52eac20c0d5f4fbc4d126aad8d
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-11-26 21:14:51 +0330

    Task.wait Used and tested

---
+++ TaskWait/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("*** Main Start ***");
+
+Task[] tasks = new Task[3]
+{
+    Task.Factory.StartNew(() => Method1()),
+    Task.Factory.StartNew(() => Method2()),
+    Task.Factory.StartNew(() => Method3())
+};
+
+// Task.WaitAny(tasks);
+Task.WaitAll(tasks);
+
+Console.WriteLine("*** Main End ***");
+Console.ReadKey();
+
+static void Method1()
+{
+    Thread.Sleep(2000);
+    Console.WriteLine("Method1");
+}
+
+static void Method2()
+{
+    Console.WriteLine("Method2");
+}
+
+static void Method3()
+{
+    Console.WriteLine("Method3");
+}
