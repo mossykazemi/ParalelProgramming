57b4b32044e05d7f973a73e5a30f5a440642ec98
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-11-29 22:02:24 +0330

    Composing Tasks

---
+++ ComposingTask/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("*** Main Start ***");
+ContinueWithOperation();
+Console.WriteLine("*** Main End ***");
+Console.ReadKey();
+
+static void ContinueWithOperation()
+{
+    var t = Task<string>.Run(() => LongRunningOperation("Continue With", 3000));
+
+    t.ContinueWith((task1) => { Console.WriteLine(task1.Result); });
+}
+
+static string LongRunningOperation(string text, int sec)
+{
+    Thread.Sleep(sec);
+    return $"{text} Completed";
+}
