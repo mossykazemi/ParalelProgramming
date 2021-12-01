5475b7f520b06c6f9c54a9f3c7bb37f279ab2a85
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-01 21:11:43 +0330

    Used Custom exceptions

---
+++ TaskExceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("*** Main Start ***");
+
+var task1 = Task.Run(() => throw new CustomException("this is an exception"));
+
+while (!task1.IsCompleted)
+{
+}
+
+if (task1.Status == TaskStatus.Faulted)
+{
+    foreach (var e in task1.Exception.InnerExceptions)
+    {
+        if (e is CustomException)
+        {
+            Console.WriteLine(e.Message);
+        }
+        else
+        {
+            throw e;
+        }
+    }
+}
+
+Console.WriteLine("*** Main End ***");
+Console.ReadKey();
+
+
+public class CustomException : Exception
+{
+    public CustomException() : base()
+    {
+    }
+
+    public CustomException(string message) : base(message)
+    {
+    }
+
+    public CustomException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
