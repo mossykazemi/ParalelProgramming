935b21184f1eff19853ff2a90743a6e7f9713b18
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-02 20:39:06 +0330

    Handle Errors using continueWith

---
+++ ErrorHandling_ContinueWith/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("*** Start Main ***");
+
+var task1 = Task.Run(() => throw new CustomException("This is and exception"))
+    .ContinueWith(
+        t =>
+        {
+            Console.WriteLine($"{t.Exception.InnerException.GetType().Name} : {t.Exception.InnerException.Message}");
+        }, TaskContinuationOptions.OnlyOnFaulted);
+
+Console.WriteLine("*** End Main ***");
+Console.ReadKey();
+
+public class CustomException : Exception
+{
+    public CustomException() : base()
+    {
+    }
+
+    public CustomException(string message) : base(message)
+    {
+    }
+
+    public CustomException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
