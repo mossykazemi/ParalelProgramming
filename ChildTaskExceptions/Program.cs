575f37f92c66011ee903b39637c3f6df8b9cc029
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-02 20:36:18 +0330

    Task Child Exceptions

---
+++ ChildTaskExceptions/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("*** Start Main ***");
+
+var task1 = Task.Factory.StartNew(() =>
+{
+    var child1 = Task.Factory.StartNew(() =>
+    {
+        var child2 = Task.Factory.StartNew(() => { throw new CustomException("Second child fault attached"); },
+            TaskCreationOptions.AttachedToParent);
+        throw new CustomException("First child fault attached");
+    }, TaskCreationOptions.AttachedToParent);
+});
+
+try
+{
+    task1.Wait();
+}
+catch (AggregateException ex)
+{
+    foreach (var e in ex.Flatten().InnerExceptions)
+    {
+        if (e is CustomException)
+        {
+            Console.WriteLine(e.Message);
+        }
+        else
+        {
+            throw e;
+        }
+    }
+}
+
+Console.WriteLine("*** End Main ***");
+Console.ReadKey();
+
+
+public class CustomException : Exception
+{
+    public CustomException() : base()
+    {
+    }
+
+    public CustomException(string message) : base(message)
+    {
+    }
+
+    public CustomException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
