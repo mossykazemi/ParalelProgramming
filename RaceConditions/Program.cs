2cafc4f904fa0b697236e35b6885140b4f2b03b2
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-04 23:28:18 +0330

    Race Condition

---
+++ RaceConditions/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("*** Start Main ***");
+
+Task T1 = Task.Factory.StartNew(PrintStar);
+Task T2 = T1.ContinueWith(a => PrintPlus());
+
+Task.WaitAll(new Task[] { T1, T2 });
+
+
+Console.WriteLine("*** End Main ***");
+Console.ReadKey();
+
+
+static void PrintStar()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(" * ");
+    }
+}
+
+static void PrintPlus()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(" + ");
+    }
+}
