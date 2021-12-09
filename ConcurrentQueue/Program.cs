d3a9e67a2a652900ecf0bf32012a37aaae81aaaf
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-09 23:46:44 +0330

    Concurrent Queue used

---
+++ ConcurrentQueue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+var queue = new ConcurrentQueue<int>();
+queue.Enqueue(10);
+queue.Enqueue(20);
+
+Console.WriteLine($"Current Queue {string.Join(", ",queue.ToArray())}");
+
+if (queue.TryPeek(out int resultPeek))
+{
+    Console.WriteLine($"Try Peek result : {resultPeek}");
+}
+
+if (queue.TryDequeue(out int resultDequeue))
+{
+    Console.WriteLine($"Try Dequeue result : {resultDequeue}");
+}
+
+Console.WriteLine($"Final Queue {string.Join(", ",queue.ToArray())}");
