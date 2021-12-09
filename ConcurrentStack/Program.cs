aa7449d65720b52f33525a870bffd1733f48064b
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-09 23:48:03 +0330

    Concurrent Stack used

---
+++ ConcurrentStack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+int[] elements = { 50, 10, 0 };
+var stack = new ConcurrentStack<int>(elements);
+Console.WriteLine(string.Join(", ", stack.ToArray()));
+
+stack.Push(100);
+Console.WriteLine(string.Join(", ", stack.ToArray()));
+
+if (stack.TryPeek(out int resultPeek))
+{
+    Console.WriteLine($"TryPeek : {resultPeek}");
+}
+
+if (stack.TryPop(out int resultPop))
+{
+    Console.WriteLine($"TryPop : {resultPop}");
+}
+
+Console.WriteLine(string.Join(", ", stack.ToArray()));
+
+Console.WriteLine("*** End Main ***");
+Console.ReadKey();
