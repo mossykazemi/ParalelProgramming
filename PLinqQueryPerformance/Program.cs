60c38fbdd3901b66a9d8fae194c46a26b93b6ec7
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-09 23:30:10 +0330

    Performance of PLinq

---
+++ PLinqQueryPerformance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("*** Start Main ***");
+
+var source = Enumerable.Range(1, 3000000);
+
+var query = source
+    .Where(n => n % 2 == 0)
+    .Select(n => n);
+
+var sw = Stopwatch.StartNew();
+foreach (var n in query)
+{
+}
+
+sw.Stop();
+Console.WriteLine($"Total query time : {sw.ElapsedMilliseconds}");
+
+
+Console.WriteLine("*** End Main ***");
+Console.ReadKey();
