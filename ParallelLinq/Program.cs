254b48d346ea6ad3ae865ea936ddb84ca76ecc5e
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-09 23:29:05 +0330

    ParallelLinq used

---
+++ ParallelLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("*** Start Main ***");
+
+var source = Enumerable.Range(1, 1000000);
+var sw = new Stopwatch();
+sw.Start();
+
+var result = source
+    .AsParallel()
+    .WithDegreeOfParallelism(2)
+    .AsOrdered()
+    .Where(n => n % 2 == 0)
+    .Select(s => s);
+
+sw.Stop();
+
+Console.WriteLine($"found {result.Count()} even numbers out of {source.Count()}, in :{sw.ElapsedMilliseconds}");
+
+Console.WriteLine("*** End Main ***");
+Console.ReadKey();
