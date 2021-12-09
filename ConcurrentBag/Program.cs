ac8a98cb719d33c9ce53f8e2291ec46a485fdb60
Mostafa Kazemi <49554562+mossykazemi@users.noreply.github.com>
2021-12-09 23:49:07 +0330

    Concurrent Bag used and tested

---
+++ ConcurrentBag/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+Console.WriteLine("*** Start Main ***");
+
+var bag = new ConcurrentBag<int>();
+bag.Add(1);
+bag.Add(2);
+bag.Add(3);
+
+int result;
+if (bag.TryPeek(out result))
+{
+    Console.WriteLine($"TryPeek : {result}");
+}
+
+if (bag.TryTake(out result))
+{
+    Console.WriteLine($"TryTake : {result}");
+}
+
+Console.WriteLine($"Final bag is: {string.Join(", ", bag.ToArray())}");
+Console.WriteLine("*** End Main ***");
+Console.ReadKey();
